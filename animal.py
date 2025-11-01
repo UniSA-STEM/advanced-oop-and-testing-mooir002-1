428022d5c982753d6095cb553e525282c2044009
Isaac <mooir002@mymail.unisa.edu.au>
2025-11-01 12:25:57 +1030

    First test commit (changed Author, ID and UN)

--- animal.py
+++ animal.py
@@ -1,8 +1,8 @@
 '''
 File: filename.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Isaac Moore
+ID: 110117290
+Username: mooir002
 This is my own work as defined by the University's Academic Integrity Policy.
 ''
