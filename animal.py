d38d818f2c79a0b36169f8ddaf40aca56099b99e
Isaac <mooir002@mymail.unisa.edu.au>
2025-11-04 20:35:40 +1030

    Adding some more to the animal class and beginning the child class of Bird

--- animal.py
+++ animal.py
@@ -10,38 +10,64 @@ This is my own work as defined by the University's Academic Integrity Policy.
 
 
 class Animal:
-    def __init__(self, name, age, species, category):
-        self.name = name
-        self.age = age
-        self.species = species
-        self.category = category
+    def __init__(self, name: str, age: int, gender: str, species: str, diet: str):
+        self.__name = name
+        self.__age = age
+        self.__gender = gender
+        self.__species = species
+        self.__diet = diet
+
+    # Getters and setters
+
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+    def get_gender(self):
+        return self.__gender
+
+    def get_species(self):
+        return self.__species
+
+    def set_species(self, new_species: str):
+        self.__species = new_species
+
+    def get_diet(self):
+        return self.__diet
+
+    # Methods
 
     def cry(self):
-        print("woof!")
+        print(f"{self.__name} is crying out")
 
     def eat(self):
-        print("munch, munch, munch")
+        print(f"{self.__name} is having a feed... *om nom nom*")
 
     def sleep(self):
-        print("Zzzzz")
+        print(f"{self.__name} is now asleep... Zzzzz...")
 
-class Mammal(Animal):
-    def __init__(self, name, age, species, category):
-        super().__init__(name, age, species, category)
 
-class Reptile(Animal):
-    def __init__(self, name, age, species, category):
-        super().__init__(name, age, species, category)
+    # Properties
+    classification = property(get_species, set_species)
+    name = property(get_name)
+    age = property(get_age)
+    gender = property(get_gender)
+    diet = property(get_diet)
 
 class Bird(Animal):
-    def __init__(self, name, age, species, category):
-        super().__init__(name, age, species, category)
+    def __init__(self, name, age, gender, species, diet, flightless: bool):
+        super().__init__(name, age, gender, species, diet)
+        self.__flightless = flightless
 
-class Parrot(Bird):
-    def __init__(self, name, age, species, category):
-        super().__init__(name, age, species, category)
+    def __str__(self):
+        if self.__flightless:
+            return f"A flightless feathered friend called {self.name}"
+        else:
+            return f"A feathered friend called {self.name}"
 
-bob = Parrot("bob", age=5, species="parrot", category="Bird")
+    def cry(self):
+        print ("Squark! Chirp! Whistle!")
 
-bob.eat()
-bob.sleep()
+    #def fly(self):
--- main.py
+++ main.py
@@ -8,4 +8,14 @@ This is my own work as defined by the University's Academic Integrity Policy.
 """
 
 
+from animal import *
+from enclosure import *
+from staff import *
 
+tiko = Bird("Tiko", 5, "Male", "Toco Toucan", "fruit", False)
+
+tiko.sleep()
+tiko.eat()
+tiko.cry()
+print(tiko.classification)
+print(tiko)
